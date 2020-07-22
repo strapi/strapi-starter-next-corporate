d3427e20209db7c2fd8294a836951b36dc9e0a7e
Rémi de Juvigny <remidejuvigny@Remis-MacBook-Pro.local>
2020-07-22 12:10:39 +0200

    Parse database string

--- back/config/database.js
+++ back/config/database.js
@@ -11,11 +11,11 @@ module.exports = ({ env }) => {
         connector: "bookshelf",
         settings: {
           client: "postgres",
-          host: env("DATABASE_HOST", "localhost"),
-          port: env.int("DATABASE_PORT", 5432),
-          database: env("DATABASE_NAME", "strapi"),
-          username: env("DATABASE_USERNAME", "strapi"),
-          password: env("DATABASE_PASSWORD", "strapi"),
+          host: dbConfig.host,
+          port: dbConfig.port,
+          database: dbConfig.database,
+          username: dbConfig.user,
+          password: dbConfig.password,
           schema: "public",
           ssl: { rejectUnauthorized: false }
         },
