b675467836022cab30f92bf07aeb7180fc2699d2
Rémi de Juvigny <remidejuvigny@Remis-MacBook-Pro.local>
2020-07-22 11:38:18 +0200

    Setup Postgres

--- back/config/database.js
+++ back/config/database.js
@@ -1,15 +1,26 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-      },
-      options: {
-        useNullAsDefault: true,
-      },
-    },
-  },
-});
+const parseDbUrl = require("parse-database-url");
+
+module.exports = ({ env }) => {
+  const dbConfig = parseDbUrl(env("DATABASE_URL"));
+  console.log(dbConfig);
+
+  return {
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "bookshelf",
+        settings: {
+          client: "postgres",
+          host: env("DATABASE_HOST", "localhost"),
+          port: env.int("DATABASE_PORT", 5432),
+          database: env("DATABASE_NAME", "strapi"),
+          username: env("DATABASE_USERNAME", "strapi"),
+          password: env("DATABASE_PASSWORD", "strapi"),
+          schema: "public",
+          ssl: { rejectUnauthorized: false }
+        },
+        options: {}
+      }
+    }
+  }
+};
