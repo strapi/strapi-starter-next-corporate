94b5764ebdc83045145486bd543177a430b41c08
Rémi de Juvigny <remidejuvigny@Remis-MacBook-Pro.local>
2020-07-23 10:53:41 +0200

    Define postgres host and port

--- back/seed.sh
+++ back/seed.sh
@@ -4,9 +4,10 @@
 # Schema translation based on http://stackoverflow.com/a/4581921/1303625.
 # Some column types are not handled (e.g blobs).
 
-SQLITE_DB_PATH=$1
-PG_DB_NAME=$2
-PG_USER_NAME=$3
+PG_HOST=$1
+PG_PORT=$2
+PG_DB_NAME=$3
+PG_USER_NAME=$4
 
 SQLITE_DUMP_FILE="dump.sql"
 
@@ -20,12 +21,12 @@ sed -i '/sqlite_sequence/d ; s/integer PRIMARY KEY AUTOINCREMENT/serial PRIMARY 
 sed -i 's/datetime/timestamp/g ; s/integer[(][^)]*[)]/integer/g ; s/text[(]\([^)]*\)[)]/varchar(\1)/g' $SQLITE_DUMP_FILE
 
 # createdb -U $PG_USER_NAME $PG_DB_NAME
-psql $PG_DB_NAME $PG_USER_NAME < $SQLITE_DUMP_FILE
+psql -h $DB_HOST -p $DB_PORT $PG_DB_NAME $PG_USER_NAME < $SQLITE_DUMP_FILE
 
 # Update Postgres sequences.
-psql $PG_DB_NAME $PG_USER_NAME -c "\ds" | grep sequence | cut -d'|' -f2 | tr -d '[:blank:]' |
+psql -h $DB_HOST -p $DB_PORT $PG_DB_NAME $PG_USER_NAME -c "\ds" | grep sequence | cut -d'|' -f2 | tr -d '[:blank:]' |
 while read sequence_name; do
   table_name=${sequence_name%_id_seq}
 
-  psql $PG_DB_NAME $PG_USER_NAME -c "select setval('$sequence_name', (select max(id) from $table_name))"
+  psql -h $DB_HOST -p $DB_PORT $PG_DB_NAME $PG_USER_NAME -c "select setval('$sequence_name', (select max(id) from $table_name))"
 done
