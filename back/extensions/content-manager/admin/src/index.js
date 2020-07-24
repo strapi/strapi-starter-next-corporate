55e9ce8a81afa38d5a09d3236803bba1231685a1
Rémi de Juvigny <remidejuvigny@Remis-MacBook-Pro.local>
2020-07-24 14:29:59 +0200

    Fix Strapi 3.1.1 plugin

--- back/extensions/content-manager/admin/src/index.js
+++ back/extensions/content-manager/admin/src/index.js
@@ -4,15 +4,16 @@
 // Here's the file: strapi/docs/3.0.0-beta.x/guides/registering-a-field-in-admin.md
 // Also the strapi-generate-plugins/files/admin/src/index.js needs to be updated
 // IF THE DOC IS NOT UPDATED THE PULL REQUEST WILL NOT BE MERGED
-
 import pluginPkg from '../../package.json';
 import pluginId from './pluginId';
 import pluginLogo from './assets/images/logo.svg';
 import App from './containers/Main';
+
+import ConfigureViewButton from './InjectedComponents/ContentTypeBuilder/ConfigureViewButton';
+import PreviewURL from './InjectedComponents/PreviewURL';
 import lifecycles from './lifecycles';
 import reducers from './reducers';
 import trads from './translations';
-import PreviewURL from './components/PreviewURL';
 
 export default strapi => {
   const pluginDescription = pluginPkg.strapi.description || pluginPkg.description;
@@ -25,6 +26,12 @@ export default strapi => {
     initializer: null,
     injectedComponents: [
       {
+        plugin: 'content-type-builder.listView',
+        area: 'list.link',
+        component: ConfigureViewButton,
+        key: 'content-manager.link',
+      },
+      {
         plugin: "content-manager.editView",
         area: "right.links",
         component: PreviewURL,
@@ -32,19 +39,15 @@ export default strapi => {
         props: {},
       },
     ],
-    isReady: false,
+    isReady: true,
     isRequired: pluginPkg.strapi.required || false,
     layout: null,
     lifecycles,
-    leftMenuLinks: [],
-    leftMenuSections: [],
     mainComponent: App,
     name: pluginPkg.strapi.name,
     pluginLogo,
     preventComponentRendering: false,
     reducers,
-    suffixUrl: () => '/ctm-configurations/models',
-    suffixUrlToReplaceForLeftMenuHighlight: '/models',
     trads,
   };
 
--- front/pages/api/preview.js
+++ front/pages/api/preview.js
@@ -3,7 +3,7 @@ import { getPageData } from "utils/api";
 export default async (req, res) => {
   // Check the secret and next parameters
   // This secret should only be known to this API route and the CMS
-  if (req.query.secret !== process.env.STRAPI_PREVIEW_SECRET) {
+  if (req.query.secret !== process.env.PREVIEW_SECRET) {
     return res.status(401).json({ message: "Invalid token" });
   }
 
