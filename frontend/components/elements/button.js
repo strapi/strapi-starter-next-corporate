936ec2de29752680ca13e0a690b9e797b0ff5851
Mark Kaylor <mark.kaylor@strapi.io>
2021-01-28 18:14:46 +0100

    Add error handling

--- frontend/components/elements/button.js
+++ frontend/components/elements/button.js
@@ -2,13 +2,42 @@ import classNames from "classnames";
 import PropTypes from "prop-types";
 import { buttonLinkPropTypes } from "utils/types";
 
-const Button = ({ button, appearance, compact = false, handleClick }) => {
+const Button = ({ button, appearance, compact = false, handleClick, loading = false }) => {
+  
+  const isLoading = () => {
+    if (loading) {
+      return (
+        <svg
+          viewBox="0 0 38 38"
+          className="animate-spin h-5 w-5 stroke-current text-black-600 mr-2"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g fill="none" fillRule="evenodd">
+            <g transform="translate(1 1)" strokeWidth="2">
+              <circle strokeOpacity=".5" cx="18" cy="18" r="18" />
+              <path d="M36 18c0-9.94-8.06-18-18-18">
+                <animateTransform
+                  attributeName="transform"
+                  type="rotate"
+                  from="0 18 18"
+                  to="360 18 18"
+                  dur="1s"
+                  repeatCount="indefinite"
+                />
+              </path>
+            </g>
+          </g>
+        </svg>
+      );
+    }
+  };
+
   return (
     <button link={button} onClick={handleClick}>
       <div
         className={classNames(
           // Common classes
-          "block w-full lg:w-auto text-center uppercase tracking-wide font-semibold text-base md:text-sm border-2 rounded-md",
+          "flex w-full lg:w-auto text-center uppercase tracking-wide font-semibold text-base md:text-sm border-2 rounded-md",
           // Full-size button
           {
             "px-8 py-4": compact === false,
@@ -19,11 +48,13 @@ const Button = ({ button, appearance, compact = false, handleClick }) => {
           },
           // Specific to when the button is fully dark
           {
-            "bg-primary-600 text-white border-primary-600": appearance === "dark",
+            "bg-primary-600 text-white border-primary-600":
+              appearance === "dark",
           },
           // Specific to when the button is dark outlines
           {
-            "text-primary-600 border-primary-600": appearance === "dark-outline",
+            "text-primary-600 border-primary-600":
+              appearance === "dark-outline",
           },
           // Specific to when the button is fully white
           {
@@ -35,7 +66,7 @@ const Button = ({ button, appearance, compact = false, handleClick }) => {
           }
         )}
       >
-        {button.text}
+        {isLoading()} {button.text}
       </div>
     </button>
   );
--- frontend/components/sections/lead-form.js
+++ frontend/components/sections/lead-form.js
@@ -1,37 +1,61 @@
-import Markdown from "react-markdown";
-import ButtonLink from "../elements/button-link";
-import Image from "../elements/image";
 import Button from "../elements/button";
-import { getButtonAppearance } from "utils/button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { fetchAPI } from "utils/api";
+import { nominalTypeHack } from "prop-types";
 
 const LeadForm = ({ data }) => {
-  const [email, setEmail] = useState('');
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const handleClick = () => {
+  // Regex for valid email
+  const regEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  const handleClick = async () => {
+    setLoading(true);
     // Save submission to Strapi
-    fetchAPI('/lead-form-submissions', {
-      method: 'POST',
-      body: JSON.stringify({
-        email,
-        location: data.location,
-      }),
-    });
+    try {
+      await fetchAPI("/lead-form-submissions", {
+        method: "POST",
+        body: JSON.stringify({
+          email,
+          location: data.location,
+        }),
+      });
+    } catch (err) {
+      setError(err.message)
+    }
+    setLoading(false);
   };
+
+  useEffect(() => {
+    setError("");
+    if (email.length > 0 && !regEmail.test(email)) {
+      setError("Please enter a valid email");
+    }
+  }, [email]);
 
   return (
     <div className="py-10 text-center">
       <h1 className="text-3xl mb-10 font-bold mb-2">{data.title}</h1>
-      <div className="container flex flex-row justify-center flex-wrap gap-4">
-        <input
-          className="text-base focus:outline-none px-4 border-2 rounded-md"
-          type="email"
-          placeholder={data.emailPlaceholder}
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Button button={data.submitButton} handleClick={handleClick} />
+      <div className="flex flex-col items-center flex-wrap ">
+        <div>
+          <div className="flex gap-4">
+            <input
+              className="text-base focus:outline-none px-4 border-2 rounded-md"
+              type="email"
+              placeholder={data.emailPlaceholder}
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Button
+              button={data.submitButton}
+              loading={loading}
+              handleClick={handleClick}
+            />
+          </div>
+          <p className="text-red-500 h-12 text-sm mt-1 ml-2 text-left">{error}</p>
+        </div>
       </div>
     </div>
   );
