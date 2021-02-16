6456199784aef01f5f6dabf77f127123a4b6c09b
Mark Kaylor <mark.kaylor@strapi.io>
2021-02-16 15:37:13 +0100

    Move loader to component file

--- frontend/components/elements/button.js
+++ frontend/components/elements/button.js
@@ -1,37 +1,9 @@
 import classNames from "classnames";
 import PropTypes from "prop-types";
 import { buttonLinkPropTypes } from "utils/types";
+import Loader from './loader' 
 
 const Button = ({ button, appearance, compact = false, handleClick, loading = false }) => {
-  
-  const isLoading = () => {
-    if (loading) {
-      return (
-        <svg
-          viewBox="0 0 38 38"
-          className="animate-spin h-5 w-5 stroke-current text-black-600 mr-2"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <g fill="none" fillRule="evenodd">
-            <g transform="translate(1 1)" strokeWidth="2">
-              <circle strokeOpacity=".5" cx="18" cy="18" r="18" />
-              <path d="M36 18c0-9.94-8.06-18-18-18">
-                <animateTransform
-                  attributeName="transform"
-                  type="rotate"
-                  from="0 18 18"
-                  to="360 18 18"
-                  dur="1s"
-                  repeatCount="indefinite"
-                />
-              </path>
-            </g>
-          </g>
-        </svg>
-      );
-    }
-  };
-
   return (
     <button link={button} onClick={handleClick}>
       <div
@@ -66,7 +38,8 @@ const Button = ({ button, appearance, compact = false, handleClick, loading = fa
           }
         )}
       >
-        {isLoading()} {button.text}
+        {loading && <Loader />}
+        {button.text}
       </div>
     </button>
   );
