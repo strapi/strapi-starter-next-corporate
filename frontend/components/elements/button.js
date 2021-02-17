77daee4283e032fee802bf6237e8da4e04522a4a
Mark Kaylor <mark.kaylor@strapi.io>
2021-02-17 14:48:45 +0100

    Add yup validation with formik

--- frontend/components/elements/button.js
+++ frontend/components/elements/button.js
@@ -1,40 +1,47 @@
-import classNames from "classnames";
-import PropTypes from "prop-types";
-import { buttonLinkPropTypes } from "utils/types";
-import Loader from './loader' 
+import classNames from 'classnames'
+import PropTypes from 'prop-types'
+import { buttonLinkPropTypes } from 'utils/types'
+import Loader from './loader'
 
-const Button = ({ button, appearance, compact = false, handleClick, loading = false }) => {
+const Button = ({
+  button,
+  appearance,
+  compact = false,
+  handleClick,
+  loading = false,
+  type,
+}) => {
   return (
-    <button link={button} onClick={handleClick}>
+    <button link={button} onClick={handleClick} type={type}>
       <div
         className={classNames(
           // Common classes
-          "flex w-full lg:w-auto text-center uppercase tracking-wide font-semibold text-base md:text-sm border-2 rounded-md",
+          'flex w-full justify-center lg:w-auto text-center uppercase tracking-wide font-semibold text-base md:text-sm border-2 rounded-md',
           // Full-size button
           {
-            "px-8 py-4": compact === false,
+            'px-8 py-4': compact === false,
           },
           // Compact button
           {
-            "px-6 py-2": compact === true,
+            'px-6 py-2': compact === true,
           },
           // Specific to when the button is fully dark
           {
-            "bg-primary-600 text-white border-primary-600":
-              appearance === "dark",
+            'bg-primary-600 text-white border-primary-600':
+              appearance === 'dark',
           },
           // Specific to when the button is dark outlines
           {
-            "text-primary-600 border-primary-600":
-              appearance === "dark-outline",
+            'text-primary-600 border-primary-600':
+              appearance === 'dark-outline',
           },
           // Specific to when the button is fully white
           {
-            "bg-white text-primary-600 border-white": appearance === "white",
+            'bg-white text-primary-600 border-white': appearance === 'white',
           },
           // Specific to when the button is white outlines
           {
-            "text-white border-white": appearance === "white-outline",
+            'text-white border-white': appearance === 'white-outline',
           }
         )}
       >
@@ -42,18 +49,18 @@ const Button = ({ button, appearance, compact = false, handleClick, loading = fa
         {button.text}
       </div>
     </button>
-  );
-};
+  )
+}
 
 Button.propTypes = {
   button: buttonLinkPropTypes,
   appearance: PropTypes.oneOf([
-    "dark",
-    "white-outline",
-    "white",
-    "dark-outline",
+    'dark',
+    'white-outline',
+    'white',
+    'dark-outline',
   ]),
   compact: PropTypes.bool,
-};
+}
 
-export default Button;
+export default Button
--- frontend/utils/api.js
+++ frontend/utils/api.js
@@ -1,7 +1,7 @@
 export function getStrapiURL(path) {
   return `${
-    process.env.NEXT_PUBLIC_STRAPI_API_URL || "http://localhost:1337"
-  }${path}`;
+    process.env.NEXT_PUBLIC_STRAPI_API_URL || 'http://localhost:1337'
+  }${path}`
 }
 
 // Helper to make GET requests to Strapi
@@ -9,40 +9,41 @@ export function getStrapiURL(path) {
 export async function fetchAPI(path, options = {}) {
   const defaultOptions = {
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
-  };
+  }
   const mergedOptions = {
     ...defaultOptions,
     ...options,
-  };
-  const requestUrl = getStrapiURL(path);
-  const response = await fetch(requestUrl, mergedOptions);
+  }
+  const requestUrl = getStrapiURL(path)
+  const response = await fetch(requestUrl, mergedOptions)
 
   if (!response.ok) {
-    throw new Error(`An error occured please try again`);
+    console.error(response.statusText)
+    throw new Error(`An error occured please try again`)
   }
-  const data = await response.json();
-  return data;
+  const data = await response.json()
+  return data
 }
 
 export async function getPageData(slug, preview = false) {
   // Find the pages that match this slug
   const pagesData = await fetchAPI(
-    `/pages?slug=${slug}&status=published${preview ? "&status=draft" : ""}`
-  );
+    `/pages?slug=${slug}&status=published${preview ? '&status=draft' : ''}`
+  )
 
   // Make sure we found something, otherwise return null
   if (pagesData == null || pagesData.length === 0) {
-    return null;
+    return null
   }
 
   // Return the first item since there should only be one result per slug
-  return pagesData[0];
+  return pagesData[0]
 }
 
 // Get site data from Strapi (metadata, navbar, footer...)
 export async function getGlobalData() {
-  const global = await fetchAPI("/global");
-  return global;
+  const global = await fetchAPI('/global')
+  return global
 }
