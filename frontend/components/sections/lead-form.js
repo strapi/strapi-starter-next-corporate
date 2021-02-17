372afe9dee0645ad519cdb7e6d4ed23e9e9d15ed
Mark Kaylor <mark.kaylor@strapi.io>
2021-02-17 14:48:45 +0100

    Add yup validation with formik

--- frontend/components/sections/lead-form.js
+++ frontend/components/sections/lead-form.js
@@ -1,64 +1,72 @@
-import Button from "../elements/button";
-import { useEffect, useState } from "react";
-import { fetchAPI } from "utils/api";
-import { nominalTypeHack } from "prop-types";
+import Button from '../elements/button'
+import { useState } from 'react'
+import { fetchAPI } from 'utils/api'
+import * as yup from 'yup'
+import { Formik, Form, Field } from 'formik'
 
 const LeadForm = ({ data }) => {
-  const [email, setEmail] = useState("");
-  const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false)
 
-  // Regex for valid email
-  const regEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-  const handleClick = async () => {
-    setLoading(true);
-    // Save submission to Strapi
-    try {
-      await fetchAPI("/lead-form-submissions", {
-        method: "POST",
-        body: JSON.stringify({
-          email,
-          location: data.location,
-        }),
-      });
-    } catch (err) {
-      setError(err.message)
-    }
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    setError("");
-    if (email.length > 0 && !regEmail.test(email)) {
-      setError("Please enter a valid email");
-    }
-  }, [email]);
+  const LeadSchema = yup.object().shape({
+    email: yup.string().email().required(),
+  })
 
   return (
     <div className="py-10 text-center">
       <h1 className="text-3xl mb-10 font-bold mb-2">{data.title}</h1>
-      <div className="flex flex-col items-center flex-wrap ">
-        <div>
-          <div className="flex gap-4">
-            <input
-              className="text-base focus:outline-none px-4 border-2 rounded-md"
-              type="email"
-              placeholder={data.emailPlaceholder}
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <Button
-              button={data.submitButton}
-              loading={loading}
-              handleClick={handleClick}
-            />
-          </div>
-          <p className="text-red-500 h-12 text-sm mt-1 ml-2 text-left">{error}</p>
-        </div>
+      <div className="flex flex-col items-center">
+        <Formik
+          initialValues={{ email: '' }}
+          validationSchema={LeadSchema}
+          onSubmit={async (values, { setSubmitting, setErrors }) => {
+            setLoading(true)
+
+            try {
+              setErrors({ api: null })
+              await fetchAPI('/lead-form-submissions', {
+                method: 'POST',
+                body: JSON.stringify({
+                  email: values.email,
+                  location: data.location,
+                }),
+              })
+            } catch (err) {
+              setErrors({ api: err.message })
+            }
+
+            setLoading(false)
+            setSubmitting(false)
+          }}
+        >
+          {({
+            errors,
+            touched,
+            isSubmitting,
+          }) => (
+            <div>
+              <Form className="flex flex-col md:flex-row gap-4">
+                <Field
+                  className="text-base focus:outline-none py-4 md:py-0 px-4 border-2 rounded-md"
+                  type="email"
+                  name="email"
+                  placeholder={data.emailPlaceholder}
+                />
+                <Button
+                  type="submit"
+                  button={data.submitButton}
+                  disabled={isSubmitting}
+                  loading={loading}
+                />
+              </Form>
+              <p className="text-red-500 h-12 text-sm mt-1 ml-2 text-left">
+                {(errors.email && touched.email && errors.email) || errors.api}
+              </p>
+            </div>
+          )}
+        </Formik>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default LeadForm;
+export default LeadForm
