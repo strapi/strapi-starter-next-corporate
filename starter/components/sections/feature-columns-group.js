a89a2d5b03da7b7a11cd7c32a3668dbf61c8da5a
markkaylor <mark.kaylor@strapi.io>
2021-07-13 09:44:27 +0200

    add next image component (#44)

--- starter/components/sections/feature-columns-group.js
+++ starter/components/sections/feature-columns-group.js
@@ -1,17 +1,19 @@
-import Image from "../elements/image";
+import Image from "../elements/image"
 
 const FeatureColumnsGroup = ({ data }) => {
   return (
     <div className="container flex flex-col lg:flex-row lg:flex-wrap gap-12 align-top py-12">
       {data.features.map((feature) => (
         <div className="flex-1 text-lg" key={feature.id}>
-          <Image media={feature.icon} className="w-10" />
+          <div className="w-10 h-10">
+            <Image media={feature.icon} />
+          </div>
           <h3 className="font-bold mt-4 mb-4">{feature.title}</h3>
           <p>{feature.description}</p>
         </div>
       ))}
     </div>
-  );
-};
+  )
+}
 
-export default FeatureColumnsGroup;
+export default FeatureColumnsGroup
